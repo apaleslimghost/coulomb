858fe133d094c91ea2ba43a5ad3a43fe69ebb037
Matt Brennan <matt@153.io>
2018-02-15 14:37:58 +0000

    css

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import htmr from 'htmr';
 class App extends Component {
 	state = {
 		html: '',
+		css: '',
 	};
 
 	render() {
 		return <div>
 			<textarea onChange={e => this.setState({html: e.target.value})} value={this.state.html} />
+			<textarea onChange={e => this.setState({css: e.target.value})} value={this.state.css} />
 			<div>{htmr(this.state.html)}</div>
+			<style dangerouslySetInnerHTML={{__html: this.state.css}} />
 		</div>;
 	}
 }
